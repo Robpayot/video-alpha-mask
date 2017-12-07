0db7b23be19ebe2360ee6c28914f8cf076b92ce1
Robin Payot <robpayot@gmail.com>
2017-12-07 23:28:49 +0100

    fix Firefox keyCode

--- dist/main.js
+++ dist/main.js
@@ -16,12 +16,7 @@ var VideoAlphaMask = function () {
 		this.onChangeSize = this.onChangeSize.bind(this);
 		this.onChangeColor = this.onChangeColor.bind(this);
 
-		// a small GUI
-		this.setGui();
-
-		this.textHeight = this.controller.size; // need a real calcul
-		this.text = 'T Y P E  H E R E';
-
+		// DOM selectors
 		this.ui = {
 			canvas: document.querySelector('.canvas'),
 			canvasAlphaBuffer: document.querySelector('.canvas-alpha-buffer'),
@@ -31,6 +26,9 @@ var VideoAlphaMask = function () {
 		this.ctx = this.ui.canvas.getContext('2d');
 		this.ctxAlphaBuffer = this.ui.canvasAlphaBuffer.getContext('2d');
 
+		this.text = 'T Y P E  H E R E'; // default text value
+
+		this.setGui();
 		this.setAlphaVideo();
 		this.resizeHandler();
 
@@ -40,10 +38,16 @@ var VideoAlphaMask = function () {
 	_createClass(VideoAlphaMask, [{
 		key: 'setGui',
 		value: function setGui() {
+			var _this = this;
+
+			// a small GUI for controls
 			this.controller = {
 				size: 150,
 				color: '#000000',
-				background: '#FFFFFF'
+				background: '#FFFFFF',
+				clear: function clear() {
+					_this.text = '';
+				}
 			};
 
 			var gui = new dat.GUI();
@@ -51,6 +55,7 @@ var VideoAlphaMask = function () {
 			gui.add(this.controller, 'size', 0, 300).onChange(this.onChangeSize);
 			gui.addColor(this.controller, 'color').onChange(this.onChangeColor);
 			gui.addColor(this.controller, 'background').onChange(this.onChangeBackground);
+			gui.add(this.controller, 'clear');
 			gui.open();
 		}
 	}, {
@@ -76,13 +81,13 @@ var VideoAlphaMask = function () {
 		key: 'setAlphaVideo',
 		value: function setAlphaVideo() {
 
+			// we create a video element. It contains our video alpha
 			this.video = document.createElement('video');
 
-			this.video.src = 'dist/video-alpha.mp4';
+			this.video.src = 'dist/video-alpha.mp4'; // We need to create a video alpha (exemple on READ.ME)
 			this.video.autoplay = true;
 			this.video.loop = true;
 			this.video.muted = true;
-			this.video.crossOrigin = 'Anonymous';
 		}
 	}, {
 		key: 'events',
@@ -98,17 +103,23 @@ var VideoAlphaMask = function () {
 		key: 'onKeyPress',
 		value: function onKeyPress(e) {
 
-			if (this.firstKeypress !== true) {
-				this.firstKeypress = true;
-				this.text = String.fromCharCode(e.keyCode);
-			} else {
-				this.text += String.fromCharCode(e.keyCode);
+			var code = e.charCode || e.keyCode; // e.charCode for FireFox
+
+			if (String.fromCharCode(code) && String.fromCharCode(code) !== '') {
+				if (this.firstKeypress !== true) {
+					this.firstKeypress = true;
+					this.text = String.fromCharCode(code);
+				} else {
+					this.text += String.fromCharCode(code);
+				}
 			}
 		}
 	}, {
 		key: 'onKeyDown',
 		value: function onKeyDown(e) {
-			if (e.keyCode === 8) {
+
+			var code = e.charCode || e.keyCode; // e.charCode for FireFox
+			if (code === 8) {
 				this.text = this.text.slice(0, -1);
 			}
 		}
@@ -117,7 +128,7 @@ var VideoAlphaMask = function () {
 		value: function resizeHandler() {
 
 			this.width = Math.min(window.innerWidth * 0.5 * 2, 1400); // x2 display
-			this.height = this.controller.size + 80; // marge
+			this.height = this.controller.size + 80; // text height + marge
 
 			// Video size
 			this.videoWidth = this.width;
@@ -126,15 +137,13 @@ var VideoAlphaMask = function () {
 			this.ui.canvasAlphaBuffer.width = this.videoWidth;
 			this.ui.canvasAlphaBuffer.height = this.videoHeight;
 
-			this.font = this.controller.size + 'px "Lato"'; // Theinhardt
-			this.textWidth = Math.round(this.ctx.measureText(this.text).width);
-			this.textHeight = Math.round(this.ctx.measureText(this.text).height);
+			this.font = this.controller.size + 'px "Lato"'; // Font
 
-			this.ui.canvas.height = this.height;
+			this.ui.canvas.height = this.height; // set video canvas retina (size x2)
 			this.ui.canvas.width = this.width;
 
-			TweenMax.set(this.ui.canvas, { width: this.width / 2 }); // x2 display
-			TweenMax.set(this.ui.canvas, { height: this.height / 2 }); // x2 display
+			TweenMax.set(this.ui.canvas, { width: this.width / 2 }); // add style for css pixel size x1
+			TweenMax.set(this.ui.canvas, { height: this.height / 2 });
 
 			this.ctx.font = this.font;
 		}
@@ -146,29 +155,26 @@ var VideoAlphaMask = function () {
 			this.ctx.clearRect(0, 0, this.ui.canvas.width, this.ui.canvas.height);
 
 			// alpha video
-			this.ctxAlphaBuffer.beginPath();
-			this.ctxAlphaBuffer.drawImage(this.video, 0, 0, this.videoWidth, this.videoHeight); // 
-			this.imageAlpha = this.ctxAlphaBuffer.getImageData(0, 0, this.videoWidth, this.videoHeight / 2); // --> top part of video
+			// draw images per images in our canvas Buffer
+			this.ctxAlphaBuffer.drawImage(this.video, 0, 0, this.videoWidth, this.videoHeight);
+			// get datas of image
+			this.imageAlpha = this.ctxAlphaBuffer.getImageData(0, 0, this.videoWidth, this.videoHeight / 2); // top part of video 
 			this.imageData = this.imageAlpha.data;
 			this.alphaData = this.ctxAlphaBuffer.getImageData(0, this.videoHeight / 2, this.videoWidth, this.videoHeight / 2).data; // --> bottom part 50/50
 			// We select the second half
 			// we apply alpha
-			this.imageDataLenght = this.imageData.length; // --> cached data for perf
-			for (var i = 3; i < this.imageDataLenght; i += 4) {
-				// why 3 and 4 ? RGB ?
-				this.imageData[i] = this.alphaData[i - 1];
+			this.imageDataLength = this.imageAlpha.data.length; // --> cached data length for perf
+			for (var i = 3; i < this.imageDataLength; i += 4) {
+				// iterate 4 for RGBA values
+				this.imageAlpha.data[i] = this.alphaData[i - 1];
 			}
 
-			this.ctx.beginPath();
-
-			this.ctx.fillStyle = this.controller.color;
+			this.ctx.fillStyle = this.controller.color; // color fill
 			this.ctx.putImageData(this.imageAlpha, 0, 0, 0, 0, this.width, this.height);
-			this.ctx.globalCompositeOperation = 'source-in';
+			this.ctx.globalCompositeOperation = 'source-in'; // use a source-in composition for mask
 
-			this.ctx.font = this.font;
 			this.ctx.textAlign = 'center';
-			this.ctx.fillText(this.text, this.width / 2, this.height / 2 + this.controller.size / 2); // First Text
-			this.ctx.font = this.font;
+			this.ctx.fillText(this.text, this.width / 2, this.height / 2 + this.controller.size / 2); // Fill with text
 		}
 	}]);
 
--- source/VideoAlphaMask.js
+++ source/VideoAlphaMask.js
@@ -1,7 +1,7 @@
 class VideoAlphaMask {
 
 	constructor() {
-		
+
 		// bind
 		this.render = this.render.bind(this);
 		this.resizeHandler = this.resizeHandler.bind(this);
@@ -10,13 +10,7 @@ class VideoAlphaMask {
 		this.onChangeSize = this.onChangeSize.bind(this);
 		this.onChangeColor = this.onChangeColor.bind(this);
 
-		
-		// a small GUI
-		this.setGui()
-
-		this.textHeight = this.controller.size; // need a real calcul
-		this.text = 'T Y P E  H E R E';
-
+		// DOM selectors
 		this.ui = {
 			canvas: document.querySelector('.canvas'),
 			canvasAlphaBuffer: document.querySelector('.canvas-alpha-buffer'),
@@ -26,6 +20,9 @@ class VideoAlphaMask {
 		this.ctx = this.ui.canvas.getContext('2d');
 		this.ctxAlphaBuffer = this.ui.canvasAlphaBuffer.getContext('2d');
 
+		this.text = 'T Y P E  H E R E'; // default text value
+
+		this.setGui();
 		this.setAlphaVideo();
 		this.resizeHandler();
 
@@ -34,10 +31,15 @@ class VideoAlphaMask {
 	}
 
 	setGui() {
+
+		// a small GUI for controls
 		this.controller = {
 			size: 150,
 			color: '#000000',
-			background: '#FFFFFF'
+			background: '#FFFFFF',
+			clear: () => {
+				this.text = '';
+			}
 		};
 
 		const gui = new dat.GUI();
@@ -45,6 +47,7 @@ class VideoAlphaMask {
 		gui.add(this.controller, 'size', 0, 300).onChange(this.onChangeSize);
 		gui.addColor(this.controller, 'color').onChange(this.onChangeColor);
 		gui.addColor(this.controller, 'background').onChange(this.onChangeBackground);
+		gui.add(this.controller, 'clear');
 		gui.open();
 	}
 
@@ -67,13 +70,13 @@ class VideoAlphaMask {
 
 	setAlphaVideo() {
 
+		// we create a video element. It contains our video alpha
 		this.video = document.createElement('video');
 
-		this.video.src = 'dist/video-alpha.mp4';
+		this.video.src = 'dist/video-alpha.mp4'; // We need to create a video alpha (exemple on READ.ME)
 		this.video.autoplay = true;
 		this.video.loop = true;
 		this.video.muted = true;
-		this.video.crossOrigin = 'Anonymous';
 
 	}
 
@@ -89,17 +92,23 @@ class VideoAlphaMask {
 
 	onKeyPress(e) {
 
-		if (this.firstKeypress !== true ) {
-			this.firstKeypress = true;
-			this.text = String.fromCharCode(e.keyCode);
-		} else {
-			this.text += String.fromCharCode(e.keyCode);
+		let code = e.charCode || e.keyCode; // e.charCode for FireFox
+
+		if (String.fromCharCode(code) && String.fromCharCode(code) !== '') {
+			if (this.firstKeypress !== true ) {
+				this.firstKeypress = true;
+				this.text = String.fromCharCode(code);
+			} else {
+				this.text += String.fromCharCode(code);
+			}
 		}
 
 	}
 
 	onKeyDown(e) {
-		if (e.keyCode === 8 ) {
+
+		let code = e.charCode || e.keyCode; // e.charCode for FireFox
+		if (code === 8 ) {
 			this.text = this.text.slice(0,-1);
 		}
 	}
@@ -109,7 +118,7 @@ class VideoAlphaMask {
 
 
 		this.width = Math.min(window.innerWidth * 0.5 * 2, 1400); // x2 display
-		this.height = this.controller.size + 80; // marge
+		this.height = this.controller.size + 80; // text height + marge
 
 		// Video size
 		this.videoWidth = this.width;
@@ -118,15 +127,13 @@ class VideoAlphaMask {
 		this.ui.canvasAlphaBuffer.width = this.videoWidth;
 		this.ui.canvasAlphaBuffer.height = this.videoHeight;
 
-		this.font = `${this.controller.size}px "Lato"`; // Theinhardt
-		this.textWidth = Math.round((this.ctx.measureText(this.text)).width);
-		this.textHeight = Math.round((this.ctx.measureText(this.text)).height);
+		this.font = `${this.controller.size}px "Lato"`; // Font
 
-		this.ui.canvas.height = this.height;
+		this.ui.canvas.height = this.height; // set video canvas retina (size x2)
 		this.ui.canvas.width = this.width;
 
-		TweenMax.set(this.ui.canvas, {width: this.width / 2}); // x2 display
-		TweenMax.set(this.ui.canvas, {height: this.height / 2}); // x2 display
+		TweenMax.set(this.ui.canvas, {width: this.width / 2}); // add style for css pixel size x1
+		TweenMax.set(this.ui.canvas, {height: this.height / 2});
 
 		this.ctx.font = this.font;
 
@@ -139,30 +146,28 @@ class VideoAlphaMask {
 		this.ctx.clearRect(0, 0, this.ui.canvas.width, this.ui.canvas.height);
 
 		// alpha video
-		this.ctxAlphaBuffer.beginPath();
-		this.ctxAlphaBuffer.drawImage(this.video, 0, 0, this.videoWidth, this.videoHeight); // 
-		this.imageAlpha = this.ctxAlphaBuffer.getImageData(0, 0, this.videoWidth, this.videoHeight / 2); // --> top part of video
+		// draw images per images in our canvas Buffer
+		this.ctxAlphaBuffer.drawImage(this.video, 0, 0, this.videoWidth, this.videoHeight);
+		// get datas of image
+		this.imageAlpha = this.ctxAlphaBuffer.getImageData(0, 0, this.videoWidth, this.videoHeight / 2); // top part of video 
 		this.imageData = this.imageAlpha.data;
 		this.alphaData = this.ctxAlphaBuffer.getImageData(0, this.videoHeight / 2, this.videoWidth, this.videoHeight / 2).data; // --> bottom part 50/50
 		// We select the second half
 		// we apply alpha
-		this.imageDataLenght = this.imageData.length; // --> cached data for perf
-		for (let i = 3; i < this.imageDataLenght; i += 4) { // why 3 and 4 ? RGB ?
-			this.imageData[i] = this.alphaData[i - 1];
+		this.imageDataLength = this.imageAlpha.data.length; // --> cached data length for perf
+		for (let i = 3; i < this.imageDataLength; i += 4) { // iterate 4 for RGBA values
+			this.imageAlpha.data[i] = this.alphaData[i - 1];
 		}
 
-		this.ctx.beginPath(); 
 
-		this.ctx.fillStyle = this.controller.color;
+		this.ctx.fillStyle = this.controller.color; // color fill
 		this.ctx.putImageData(this.imageAlpha, 0, 0, 0, 0, this.width, this.height);
-		this.ctx.globalCompositeOperation = 'source-in';
+		this.ctx.globalCompositeOperation = 'source-in'; // use a source-in composition for mask
 
-		this.ctx.font = this.font;
 		this.ctx.textAlign = 'center';
-		this.ctx.fillText(this.text, this.width / 2, this.height / 2 + this.controller.size / 2); // First Text
-		this.ctx.font = this.font;
+		this.ctx.fillText(this.text, this.width / 2, this.height / 2 + this.controller.size / 2); // Fill with text
 
 	}
 }
 
-new VideoAlphaMask()
+new VideoAlphaMask();
